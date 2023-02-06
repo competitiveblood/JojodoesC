448ece0268fe7c45f8d24e5e22f1bf3620922769
Shivangi Singh <122040215+ShivangiS04@users.noreply.github.com>
2023-02-06 13:22:22 +0530

    Create Addition of even terms.c

---
+++ Addition of even terms.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, arr[100], sum = 0;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements of the array: ");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for (i = 0; i < n; i++) {
+        if (arr[i] % 2 == 0) {
+            sum = sum + arr[i];
+        }
+    }
+    printf("Sum of even terms: %d\n", sum);
+    return 0;
+}
