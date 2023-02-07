99c7450f0dacb5580e9a9fec1b518af08df52180
Shivangi Singh <122040215+ShivangiS04@users.noreply.github.com>
2023-02-07 12:23:34 +0530

    Create Volume of cylinder.c

---
+++ Volume of cylinder.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+#define PI 3.14159265
+
+int main()
+{
+    double radius, height, volume;
+
+    printf("Enter the radius of cylinder: ");
+    scanf("%lf", &radius);
+    printf("Enter the height of cylinder: ");
+    scanf("
