6816ea72460f5f091a2f7c0005117f811393f68a
Shivangi Singh <122040215+ShivangiS04@users.noreply.github.com>
2023-02-09 00:09:47 +0530

    Update simulate an arithmetic calculator.c

--- simulate an arithmetic calculator.c
+++ simulate an arithmetic calculator.c
@@ -22,7 +22,9 @@ int main()
   printf("Enter second number: ");
   scanf("%f", &num2);
 
-  switch (choice) {
+  switch (choice) 
+  
+  {
     case 1:
       result = num1 + num2;
       printf("%.2f + %.2f = %.2f\n", num1, num2, result);
