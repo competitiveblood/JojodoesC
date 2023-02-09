39eb302bd81f1366c2fcc8bb6c5d335b2402ae5b
Shivangi Singh <122040215+ShivangiS04@users.noreply.github.com>
2023-02-09 09:20:48 +0530

    Create Division obtained by students.c

---
+++ Division obtained by students.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+  int m1,m2,m3,m4,m5,per;
+  printf("Enter marks in five subjects");
+  scanf("%d,%d,%d,%d,%d",&m1,&m2,&m3,&m4,&m5);
+  per=(m1+m2+m3+m4+m5)*100/500;
+  if(per>=60)
+    printf("First division\n");
+  else
+  {
+    if(per>=50)
+      printf("second division");
+    else
+    {
+      if (per>=40)
+        printf("Third division");
+      else
+        printf("Fail\n");
+    }
+   
+  }
+  return 0;
+}
