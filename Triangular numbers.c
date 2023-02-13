43cfc5183f14f14a3a49c9d2b21f79d4b36b4857
Shivangi Singh <122040215+ShivangiS04@users.noreply.github.com>
2023-02-13 23:04:28 +0530

    Create Triangular numbers.c

---
+++ Triangular numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void main()
+{
+int n,i,c=0;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+c=c+i;
+if(c==n)
+{
+printf("the number is triangular");
+break;
+
+}
+if(i==n)
+{
+printf("the number is not triangular");
+}
+}
