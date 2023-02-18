ffcedc8b082d39c321f21d94dad6f97e009a6287
Shivangi Singh <122040215+ShivangiS04@users.noreply.github.com>
2023-02-18 12:09:51 +0530

    Create Counting Apples.c

---
+++ Counting Apples.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[100]; // assuming the maximum size of the string is 100
+    int m, n, countO = 0, maxCount = 0;
+    scanf("%s%d", s, &m);
+    n = strlen(s);
+    for (int i = 0; i < n; i++) {
+        if (s[i] == 'A') {
+            maxCount = (countO > maxCount) ? countO : maxCount;
+            countO = 0;
+        } else { // s[i] == 'O'
+            if (m > 0) {
+                m--;
+            } else {
+                countO++;
+            }
+            maxCount = (i == n-1 && countO > maxCount) ? countO : maxCount;
+        }
+    }
+    printf("%d\n", maxCount);
+    return 0;
