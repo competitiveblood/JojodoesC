4158f59ce748660f67c4c8d7fb9f5c60d981ea12
Shivangi Singh <122040215+ShivangiS04@users.noreply.github.com>
2023-02-21 18:56:41 +0530

    Update Covert array into zigzag fashion.c

--- Covert array into zigzag fashion.c
+++ Covert array into zigzag fashion.c
@@ -19,3 +19,20 @@ void zigzag(int arr[], int n) {
         flag = !flag; // toggle the flag after each iteration
     }
 }
+
+
+
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    zigzag(arr, n);
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
