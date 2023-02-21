422008ff7ed4059bbd3a00578c87ba260a0b2dca
Shivangi Singh <122040215+ShivangiS04@users.noreply.github.com>
2023-02-21 18:56:05 +0530

    Create Covert array into zigzag fashion.c

---
+++ Covert array into zigzag fashion.c
@@ -0,0 +1,21 @@
+void zigzag(int arr[], int n) {
+    int i, temp;
+    bool flag = true; // flag is used to indicate whether the next element should be greater or lesser
+
+    for (i = 0; i < n - 1; i++) {
+        if (flag) { // if the flag is true, we expect the next element to be greater
+            if (arr[i] > arr[i+1]) { // if the current element is greater than the next element, swap them
+                temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+            }
+        } else { // if the flag is false, we expect the next element to be lesser
+            if (arr[i] < arr[i+1]) { // if the current element is lesser than the next element, swap them
+                temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+            }
+        }
+        flag = !flag; // toggle the flag after each iteration
+    }
+}
