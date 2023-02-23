59085a0ea2da4c146c6f05c3394a7c24b74e9395
Shivangi Singh <122040215+ShivangiS04@users.noreply.github.com>
2023-02-23 11:55:26 +0530

    Create Binary search.c

---
+++ Binary search.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int left, int right, int target) {
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target) {
+            return mid;
+        }
+        else if (arr[mid] < target) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+    int arr[] = {1, 3, 5, 7, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 5;
+    int result = binarySearch(arr, 0, n - 1, target);
+
+    if (result == -1) {
+        printf("Element not found.");
+    }
+    else {
+        printf("Element found at index %d.", result);
+    }
