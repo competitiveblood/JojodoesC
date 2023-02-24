2c0a37795bd5d98ee73e059fb9e37d410282cd9d
Shivangi Singh <122040215+ShivangiS04@users.noreply.github.com>
2023-02-24 14:43:56 +0530

    Create Reverse array in group.c

---
+++ Reverse array in group.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+void reverse(int arr[], int start, int end) {
+    int temp;
+    while (start < end) {
+        temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+void reverseInGroups(int arr[], int n, int k) {
+    int i;
+    for (i = 0; i < n; i += k) {
+        if (i + k <= n) {
+            reverse(arr, i, i + k - 1);
+        } else {
+            reverse(arr, i, n - 1);
+        }
+    }
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int k = 3;
+    int i;
+    
+    printf("Original array: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    
+    reverseInGroups(arr, n, k);
+    
+    printf("Reversed array in groups of %d: ", k);
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    
